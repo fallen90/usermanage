9a11804524014b2a7e144c0ad0a5176da98d227e
fallen90 <slytherin9090@gmail.com>
2016-05-27 08:53:24 +0000

    added new endpoints, added some fixes

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
 
 	'secret': 'a1driving',
-	'database': 'mongodb://admin:admin@localhost:27017/a1driving'
+	'expiresIn' : 1800,
+    'database': 'mongodb://admin:q-DzMUkpeunG@127.0.0.1:27017/mean'
+//    'database' : 'mongodb://admin:q-DzMUkpeunG@mean-fallen90.rhcloud.com:27017/mean'
 
 };
